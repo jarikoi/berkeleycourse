07f1745567c0704f4263be6ac6705f9ed9bf47a4
dwmclary <dan.mcclary@gmail.com>
2015-10-24 23:00:38 -0700

    added updated procedures.sql

--- exercise_1/transforming/procedures.sql
+++ exercise_1/transforming/procedures.sql
@@ -1,11 +1,26 @@
+CREATE TABLE max_effective_care
+  STORED AS PARQUET
+  AS
+    select measureid, MAX(CAST(score AS FLOAT)) as max_score
+    from effective_care
+    where score RLIKE '^[0-9]+\.*[0-9]*'
+    group by measureid
+
+
 CREATE TABLE procedures
   STORED AS PARQUET
   AS
   select
-  measureid, providerid, measurename, CAST(score AS FLOAT)
-  from effective_care
+  m.measureid, m.providerid, m.measurename, CAST(m.score AS FLOAT) raw_score,
+  CAST(m.score AS FLOAT)/x.max_score relative_score
+  from effective_care m
+  join max_effective_care x
+  on m.measureid=x.measureid
   where score RLIKE '^[0-9]+\.*[0-9]*'
 union all
-  select measureid, providerid, measurename, CAST(score AS FLOAT)
-  from readmissions
+select
+  m.measureid, m.providerid, m.measurename, CAST(m.score AS FLOAT) raw_score,  1.0 - (CAST(m.score AS FLOAT)/x.max_score) relative_score
+  from readmissions m
+  join readmissions_max x
+  on m.measureid=x.measureid
   where score RLIKE '^[0-9]+\.*[0-9]*'
