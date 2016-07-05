0dd4110641175687527cd3207529f343f487581a
Jari Koister <jari@eightcut.com>
2016-07-04 17:41:19 -0700

    simple example

---
+++ lab_10/code/updatestate.py
@@ -0,0 +1,23 @@
+import sys
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+sc = SparkContext(appName="PythonStreamingStatefulNetworkWordCount")
+ssc = StreamingContext(sc, 1)
+jari koisterssc.checkpoint("checkpoint")
+initialStateRDD = sc.parallelize([(u'hello', 1), (u'world', 1)])
+def updateFunc(new_values, last_sum):
+        return sum(new_values) + (last_sum or 0)
+lines = ssc.socketTextStream("localhost", 9999)
+#running_counts = lines.flatMap(lambda line: line.split(" "))\
+#                          .map(lambda word: (word, 1))\
+#                          .updateStateByKey(updateFunc)
+
+# Splitting it up to make it easier to follow.
+# running_counts get a special status.
+
+l2 =lines.flatMap(lambda line: line.split(" "))
+l3=l2.map(lambda word: (word, 1))
+running_counts=l3.updateStateByKey(updateFunc)
+running_counts.pprint()
+ssc.start()
+ssc.awaitTermination()
